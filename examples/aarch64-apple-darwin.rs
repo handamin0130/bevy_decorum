7b74c0ec8d160d0acb7fac48922e402eddd4ca21
handamin0130 <handamin0130@gmail.com>
2024-06-23 03:07:00 +0900

    chore: fix examples for test

--- examples/aarch64-apple-darwin.rs
+++ examples/aarch64-apple-darwin.rs
@@ -5,14 +5,7 @@ fn main() {
     let mut app = App::new();
 
     // set bevy plugin
-    app.add_plugins(DefaultPlugins.set(WindowPlugin {
-        primary_window: Some(Window {
-            title: "example: arrch64-apple-darwin".to_string(),
-            decorations: false,
-            ..Default::default()
-        }),
-        ..Default::default()
-    }));
+    app.add_plugins(DefaultPlugins);
 
     // set bevy_decorum plugin
     app.add_plugins(DecorumPlugin::default());
